4a855cf587e0f05cfe1aaad2207aa362fcf4d840
Debora7 <burnichinaomi@gmail.com>
2026-02-04 11:15:51 +0200

    First try at fixing this.

--- mrp_partial_production/modules/mrp_production.py
+++ mrp_partial_production/modules/mrp_production.py
@@ -33,7 +33,7 @@ class MrpProduction(models.Model):
 
         if(produced_qty == self.product_qty):
             self.write({
-                'state': 'done',
+                'state': 'done', # TODO nu setam done din db 
             })
         
         self.write({
@@ -43,55 +43,40 @@ class MrpProduction(models.Model):
 
     @api.onchange('qty_producing')
     def _onchange_qty_producing(self):
-        _logger.info(">>>>>>>>>on changed called!!!")
-        quantity_right = self.qty_producing
-        
-        return super(MrpProduction, self.with_context(
-            skip_qty_calculation=True, 
-            quantity_right=quantity_right
-        ))._onchange_qty_producing()
-        
-    # def _change_producing(self):
-    #     _logger.info('_change_producing called')
-    #     if self.env.context.get('skip_qty_calculation'):
-    #         self = self.with_context(
-    #             skip_qty_calculation=True,
-    #             quantity_right=self.env.context.get('quantity_right')
-    #         )
-    #     return super(MrpProduction, self)._change_producing()
-    
-    # def _set_qty_producing(self, pick_manual_consumption_moves=True):
-    #     if self.product_id.tracking == 'serial':
-    #         qty_producing_uom = self.product_uom_id._compute_quantity(self.qty_producing, self.product_id.uom_id, rounding_method='HALF-UP')
-    #         qty_production_uom = self.product_uom_id._compute_quantity(self.product_qty, self.product_id.uom_id, rounding_method='HALF-UP')
-    #         # allow changing a non-zero value to a 0 to not block mass produce feature
-    #         if qty_producing_uom != qty_production_uom and not (qty_producing_uom == 0 and self._origin.qty_producing != self.qty_producing):
-    #             self.qty_producing = self.product_id.uom_id._compute_quantity(len(self.lot_producing_ids), self.product_uom_id, rounding_method='HALF-UP')
+        self.with_context(skip_qty_calculation=True)._change_producing()
 
-    #     # waiting for a preproduction move before assignement
-    #     is_waiting = self.warehouse_id.manufacture_steps != 'mrp_one_step' and self.picking_ids.filtered(lambda p: p.picking_type_id == self.warehouse_id.pbm_type_id and p.state not in ('done', 'cancel'))
+    def _set_qty_producing(self, pick_manual_consumption_moves=True):
+        if self.product_id.tracking == 'serial':
+            qty_producing_uom = self.product_uom_id._compute_quantity(self.qty_producing, self.product_id.uom_id, rounding_method='HALF-UP')
+            qty_production_uom = self.product_uom_id._compute_quantity(self.product_qty, self.product_id.uom_id, rounding_method='HALF-UP')
+            # allow changing a non-zero value to a 0 to not block mass produce feature
+            if qty_producing_uom != qty_production_uom and not (qty_producing_uom == 0 and self._origin.qty_producing != self.qty_producing):
+                self.qty_producing = self.product_id.uom_id._compute_quantity(len(self.lot_producing_ids), self.product_uom_id, rounding_method='HALF-UP')
 
-    #     for move in (
-    #         self.move_raw_ids.filtered(lambda m: not is_waiting or m.product_id.tracking == 'none')
-    #         | self.move_finished_ids.filtered(lambda m: m.product_id != self.product_id or m.product_id.tracking == 'serial')
-    #     ):
-    #         is_byproduct = move in self.move_byproduct_ids
-    #         # Never update already produced by-product moves.
-    #         if move.picked and (is_byproduct or move.manual_consumption):
-    #             continue
+        # waiting for a preproduction move before assignement
+        is_waiting = self.warehouse_id.manufacture_steps != 'mrp_one_step' and self.picking_ids.filtered(lambda p: p.picking_type_id == self.warehouse_id.pbm_type_id and p.state not in ('done', 'cancel'))
 
-    #         # sudo needed for portal users
-    #         if move.sudo()._should_bypass_set_qty_producing():
-    #             continue
+        for move in (
+            self.move_raw_ids.filtered(lambda m: not is_waiting or m.product_id.tracking == 'none')
+            | self.move_finished_ids.filtered(lambda m: m.product_id != self.product_id or m.product_id.tracking == 'serial')
+        ):
+            is_byproduct = move in self.move_byproduct_ids
+            # Never update already produced by-product moves.
+            if move.picked and (is_byproduct or move.manual_consumption):
+                continue
 
-    #         new_qty = move.product_uom.round((self.qty_producing - self.qty_produced) * move.unit_factor)
+            # sudo needed for portal users
+            if move.sudo()._should_bypass_set_qty_producing():
+                continue
 
-    #         if self.env.context.get('skip_qty_calculation'):
-    #             move._set_quantity_done(self.env.context.get('quantity_right'))
+            if self.env.context.get('skip_qty_calculation'):
+                new_qty = move.product_uom.round((self.qty_producing) * move.unit_factor)
+            else:
+                new_qty = move.product_uom.round((self.qty_producing - self.qty_produced) * move.unit_factor)
 
-    #         move._set_quantity_done(new_qty)
-    #         if (not move.manual_consumption or pick_manual_consumption_moves) \
-    #                 and move.quantity \
-    #                 and not is_byproduct \
-    #                 and (move.raw_material_production_id or move.product_id.tracking != 'serial'):
-    #             move.picked = True
+            move._set_quantity_done(new_qty)
+            if (not move.manual_consumption or pick_manual_consumption_moves) \
+                    and move.quantity \
+                    and not is_byproduct \
+                    and (move.raw_material_production_id or move.product_id.tracking != 'serial'):
+                move.picked = True
